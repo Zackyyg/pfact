a87b6cce8784397763a8d8abe8c7a1fb6ffb4f9c
Zackyyg <zackyyyy.g@gmail.com>
2023-04-06 00:30:05 -0400

    Add library functions

--- src/trial_division.rs
+++ src/trial_division.rs
@@ -1,7 +1,7 @@
-pub fn get_list_of_prime_factors() -> {
+pub fn get_list_of_prime_factors(number: u64) -> Vec<u64> {
 
 }
 
-pub fn get_lowest_prime_factor() -> {
+pub fn get_lowest_prime_factor(number: u64) -> u64{
 
 }
